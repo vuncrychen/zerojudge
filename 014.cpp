4d0480f59dedcf1871f6a45466705ba346b8390e
vuncrychen <vuncrychen2002@gmail.com>
2019-11-21 23:07:49 +0800

    Completed school.cpp

---
+++ 014.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int is_palindrome(string);
+
+int main() {
+
+    string s;
+    while (cin >> s)
+    {
+        // if (is_palindrome(s))
+        // {
+        //     cout << "yes" << endl;
+        // }
+        // else
+        // {
+        //     cout << "no" << endl;
+        // }
+    }
+
+ return 0;
+}
+
+int is_palindrome(string a){
+    int len = a.length()/2;
+}
