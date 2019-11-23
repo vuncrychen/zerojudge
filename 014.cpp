8f3416735673ee850b61d44042944596cff10976
vuncrychen <vuncrychen2002@gmail.com>
2019-11-23 08:36:58 +0800

    Completed 014.cpp

--- 014.cpp
+++ 014.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 int is_palindrome(string);
@@ -8,19 +9,42 @@ int main() {
     string s;
     while (cin >> s)
     {
-        // if (is_palindrome(s))
-        // {
-        //     cout << "yes" << endl;
-        // }
-        // else
-        // {
-        //     cout << "no" << endl;
-        // }
+        if (is_palindrome(s))
+        {
+            cout << "yes" << endl;
+        }
+        else
+        {
+            cout << "no" << endl;
+        }
     }
 
  return 0;
 }
 
 int is_palindrome(string a){
+    char diff_a[501], diff_b[501];
     int len = a.length()/2;
+    int count = 0;
+    for (int i = 0; i < len; i++)
+    {
+        diff_a[i] = a[i];
+    }
+    reverse(a.begin(), a.end());
+    for (int i = 0; i < len; i++)
+    {
+        diff_b[i] = a[i];
+    }
+    for (int i = 0; i < len; i++)
+    {
+        if (diff_a[i] == diff_b[i])
+        {
+            count += 1;
+        }
+    }
+    if (count == len)
+    {
+        return 1;
+    }
+    return 0;
 }
